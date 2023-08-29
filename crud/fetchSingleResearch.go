18d840bf36d6f522fad672d5975dea8e25fe7e69
Jon Nicholson <jonnicholson@Jons-MBP.lan>
2023-08-29 08:39:22 +0100

    Updated fetchSingleResearch function to combine data more efficiently for FE

--- crud/fetchSingleResearch.go
+++ crud/fetchSingleResearch.go
@@ -7,6 +7,40 @@ import (
 	"net/http"
 )
 
+type FinalResearch struct {
+	ID           string     `json:"ID"`
+	Title        string     `json:"title"`
+	Description  string     `json:"description"`
+	Status       string     `json:"status"`
+	Limit        int        `json:"limit"`
+	PrototypeUrl string     `json:"prototype_url"`
+	UserId       string     `json:"user_id"`
+	Questions    []Question `json:"questions"`
+}
+
+type JoinedResearch struct {
+	ID                 string `json:"ID"`
+	Title              string `json:"title"`
+	Description        string `json:"description"`
+	Status             string `json:"status"`
+	Limit              int    `json:"limit"`
+	PrototypeUrl       string `json:"prototype_url"`
+	UserId             string `json:"user_id"`
+	QuestionID         string `json:"question_id"`
+	QuestionTitle      string `json:"question_title"`
+	QuestionType       string `json:"question_type"`
+	QuestionResearchId string `json:"question_research_id"`
+	QuestionIndex      int    `json:"question_index"`
+}
+
+type Question struct {
+	QuestionID         string `json:"question_id"`
+	QuestionTitle      string `json:"question_title"`
+	QuestionType       string `json:"question_type"`
+	QuestionResearchId string `json:"question_research_id"`
+	QuestionIndex      int    `json:"question_index"`
+}
+
 func FetchSingleResearch(w http.ResponseWriter, r *http.Request) {
 
 	tk := r.Header.Get("Authorization")
@@ -14,37 +48,79 @@ func FetchSingleResearch(w http.ResponseWriter, r *http.Request) {
 	tokenErr := utils.ValidateToken(tk)
 
 	if tokenErr != nil {
+		fmt.Println(tokenErr)
 		http.Error(w, tokenErr.Error(), http.StatusUnauthorized)
 		return
 	}
 
 	id := r.URL.Query().Get("id")
 
+	// Initialise the database
+
 	db := utils.GetDB()
 
-	query := "SELECT * FROM research WHERE id = $1"
+	// Define the query
 
-	var research FetchedResearch
+	query := "SELECT research.*, questions.* FROM research JOIN questions ON research.id = questions.research_id WHERE research.id = $1;"
 
-	row := db.QueryRow(query, id)
+	var finalResearch FinalResearch
 
-	scanErr := row.Scan(
-		&research.ID,
-		&research.Title,
-		&research.Description,
-		&research.Status,
-		&research.Limit,
-		&research.PrototypeUrl,
-		&research.UserId,
-	)
+	rows, err := db.Query(query, id)
 
-	if scanErr != nil {
-		fmt.Println(scanErr)
-		http.Error(w, scanErr.Error(), http.StatusInternalServerError)
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	json, err := json.Marshal(research)
+	for rows.Next() {
+		var result JoinedResearch
+
+		scanErr := rows.Scan(
+			&result.ID,
+			&result.Title,
+			&result.Description,
+			&result.Status,
+			&result.Limit,
+			&result.PrototypeUrl,
+			&result.UserId,
+			&result.QuestionID,
+			&result.QuestionTitle,
+			&result.QuestionType,
+			&result.QuestionResearchId,
+			&result.QuestionIndex,
+		)
+
+		if scanErr != nil {
+			fmt.Println(scanErr)
+			http.Error(w, scanErr.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Append the question to the associated research's Questions slice
+		question := Question{
+			QuestionID:         result.QuestionID,
+			QuestionTitle:      result.QuestionTitle,
+			QuestionType:       result.QuestionType,
+			QuestionResearchId: result.QuestionResearchId,
+			QuestionIndex:      result.QuestionIndex,
+		}
+
+		if finalResearch.ID != result.ID {
+			finalResearch.ID = result.ID
+			finalResearch.Title = result.Description
+			finalResearch.Limit = result.Limit
+			finalResearch.Status = result.Status
+			finalResearch.PrototypeUrl = result.PrototypeUrl
+			finalResearch.UserId = result.UserId
+			finalResearch.Questions = append(finalResearch.Questions, question)
+		} else {
+			finalResearch.Questions = append(finalResearch.Questions, question)
+		}
+
+	}
+
+	json, err := json.Marshal(finalResearch)
 
 	if err != nil {
 		fmt.Println(err)
